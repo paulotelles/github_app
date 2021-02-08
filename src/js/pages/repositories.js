bba0d39bf571e591d17b243b2ac00d764bd46d1b
PauloMarxs <paulomarquestelles@gmail.com>
2021-02-08 16:53:15 -0300

    criação de toda estrutura do js para carregamento dos dados ná pagina repositories.html

--- src/js/pages/repositories.js
+++ src/js/pages/repositories.js
@@ -1,40 +1,55 @@
+//recuperando o parametro da url para utilizar
+
 let url_string = window.location.href;
 let url = new URL(url_string);
 let userName = url.searchParams.get("username");
-console.log(userName);
 
-fetch(`https://api.github.com/users/${userName}`)
-  .then((resposta) => {
-    if (resposta.ok) {
-      return resposta.json();
-    }
-    return console.log(resposta);
-  })
-  .then((json) => {
-    let userData = json;
-    console.log(userData.name);
-    console.log(userData.bio);
-    console.log(userData.followers);
-    console.log(userData.following);
-    console.log(userData.email);
-    return console.log(userData);
-  });
+//fazendo a primeira requisição com os dados necessários no sidebar.
 
-fetch(`https://api.github.com/users/${userName}/repos`)
-  .then((resposta) => {
-    if (resposta.ok) {
-      return resposta.json();
-    }
-    return console.log(resposta);
-  })
-  .then((json) => {
-    let userRepo = json;
-    json.forEach((element) => {
-      let userRepoData = element;
-      console.log(userRepoData.name);
-      console.log(userRepoData.stargazers_count);
-      console.log(userRepoData.language);
-      console.log(userRepoData.description);
-      return console.log(userRepo);
+let userData = apiRequest(`https://api.github.com/users/${userName}`).then(
+  (userData) => {
+    //função para construção do sidebar
+    createSideBar(userData);
+  }
+);
+
+//fazendo a requisão para dar carga na parte dos repositórios
+
+let userRepo = apiRequest(
+  `https://api.github.com/users/${userName}/repos`
+).then((userData) => {
+  createCard(userData);
+
+  //coloca um listener em todos os botões e recupera o dado do nome do repositorio para acesso.
+  let buttonDetails = document.querySelectorAll(".button2");
+  buttonDetails.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      let repoFullName = button.dataset.repofullname;
+      return (window.location = `./repositoriesdetails.html?repofullname=${repoFullName}`);
     });
   });
+});
+
+//
+// colocando um listener na mudança de valores do filtro de estrelas e caso tenha mudança, alterar o repositorio
+//
+
+let filterStars = document.querySelector(".starFilter");
+filterStars.addEventListener("change", () => {
+  let userRepo = apiRequest(
+    `https://api.github.com/users/${userName}/repos`
+  ).then((userData) => {
+    createCard(userData);
+
+    //coloca um listener em todos os botões e recupera o dado do nome do repositorio para acesso.
+    let buttonDetails = document.querySelectorAll(".button2");
+    buttonDetails.forEach((button) => {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        let repoFullName = button.dataset.repofullname;
+        return (window.location = `./repositoriesdetails.html?repofullname=${repoFullName}`);
+      });
+    });
+  });
+});
