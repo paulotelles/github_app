2700b79f8cab042a3b1edcc3d23cb6d179fe952c
PauloMarxs <paulomarquestelles@gmail.com>
2021-02-11 12:00:03 -0300

    criação do js da pagina de detalhes do repositorio

---
+++ src/js/pages/repositoriesdetails.js
@@ -0,0 +1,35 @@
+const { apiRequest } = require("../modules/api");
+const { createContainer } = require("../modules/container");
+
+let url_string = window.location.href;
+let url = new URL(url_string);
+let userName = url.searchParams.get("username");
+let repository = url.searchParams.get("reponame");
+
+console.log(userName);
+console.log(repository);
+
+if (userName === null || repository === null) {
+  return (window.location = `./index.html`);
+}
+
+let returnButton = document.querySelector(".returnButton");
+returnButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  return (window.location = `./repositories.html?username=${userName}`);
+});
+
+let repositoryDetails = apiRequest(
+  `https://api.github.com/repos/${userName}/${repository}`,
+  `./index.html`
+).then((repoData) => {
+  console.log(repoData);
+  createContainer(repoData);
+
+  let buttonGit = document.querySelector(".buttonGit");
+  let buttonData = buttonGit.dataset.repourl;
+  buttonGit.addEventListener("click", (event) => {
+    event.preventDefault;
+    return window.open(buttonData);
+  });
+});
